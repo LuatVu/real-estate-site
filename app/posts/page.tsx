3518b261187e589a0ede949c5755a644bf4b9899
Vu Cong Luat (MS/EET23) <vlc1hc@bosch.com>
2025-05-15 18:11:43 +0700

    adding code for post feature

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,6 +6,7 @@ import NavBarMobile from '../ui/mobile/navigation/nav-bar-mobile';
 import MbFooter from "../ui/mobile/footer/mb.footer";
 import DownloadApp from "../ui/mobile/download-app/mb.download";
 import ExtraInfo from "../ui/mobile/extra-info/mb.extra.info";
+import Link from "next/link";
 
 export default function Posts() {
     const screenSize = useScreenSize();
@@ -20,15 +21,60 @@ export default function Posts() {
 function MobilePosts() {
     return (
         <div className="h-full">
-            <div className={styles.rootContainer}>
+            <div className={styles.rootContainer}>         
                 <NavBarMobile displayNav={true} />
                 <div className={styles.postContainer}>
+                    <div className={styles.imageBlk}>
+                        <Link href={"/123423232-232"} className={styles.post}>
+                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
+                        </Link>
+                        <Link href={"/123423232-232"} className={styles.post}>
+                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
+                        </Link>
+                        <Link href={"/123423232-232"} className={styles.post}>
+                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
+                        </Link>
+                        <Link href={"/123423232-232"} className={styles.post}>
+                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
+                        </Link>                                           
+                    </div>
 
+                    <div>
+                        <h1>Nhà mới đẹp ở ngay, HTX vào nhà Nguyễn Văn Nghị P7 Gò Vấp, 3PN 7.7 tỷ</h1>
+                        <p>Thông tin mô tả</p>
+                        <p>Giảm gấp 400 triệu nhà Nguyễn Văn Nghị 4 tầng - 57m2 bán để đi định cư Mỹ cùng gia đình</p>
+                    </div>
+                    <div>
+                        <h1>Nhà mới đẹp ở ngay, HTX vào nhà Nguyễn Văn Nghị P7 Gò Vấp, 3PN 7.7 tỷ</h1>
+                        <p>Thông tin mô tả</p>
+                        <p>Giảm gấp 400 triệu nhà Nguyễn Văn Nghị 4 tầng - 57m2 bán để đi định cư Mỹ cùng gia đình</p>
+                    </div>
+                    <div>
+                        <h1>Nhà mới đẹp ở ngay, HTX vào nhà Nguyễn Văn Nghị P7 Gò Vấp, 3PN 7.7 tỷ</h1>
+                        <p>Thông tin mô tả</p>
+                        <p>Giảm gấp 400 triệu nhà Nguyễn Văn Nghị 4 tầng - 57m2 bán để đi định cư Mỹ cùng gia đình</p>
+                    </div>
+                    <div>
+                        <h1>Nhà mới đẹp ở ngay, HTX vào nhà Nguyễn Văn Nghị P7 Gò Vấp, 3PN 7.7 tỷ</h1>
+                        <p>Thông tin mô tả</p>
+                        <p>Giảm gấp 400 triệu nhà Nguyễn Văn Nghị 4 tầng - 57m2 bán để đi định cư Mỹ cùng gia đình</p>
+                    </div>
+                    <div>
+                        <h1>Nhà mới đẹp ở ngay, HTX vào nhà Nguyễn Văn Nghị P7 Gò Vấp, 3PN 7.7 tỷ</h1>
+                        <p>Thông tin mô tả</p>
+                        <p>Giảm gấp 400 triệu nhà Nguyễn Văn Nghị 4 tầng - 57m2 bán để đi định cư Mỹ cùng gia đình</p>
+                    </div>
+                    <div>
+                        <h1>Nhà mới đẹp ở ngay, HTX vào nhà Nguyễn Văn Nghị P7 Gò Vấp, 3PN 7.7 tỷ</h1>
+                        <p>Thông tin mô tả</p>
+                        <p>Giảm gấp 400 triệu nhà Nguyễn Văn Nghị 4 tầng - 57m2 bán để đi định cư Mỹ cùng gia đình</p>
+                    </div>
                 </div>
-                <ExtraInfo/>
-                <DownloadApp/>
+                <ExtraInfo />
+                <DownloadApp />
                 <MbFooter />
-            </div>            
+
+            </div>
         </div>
     );
 }
