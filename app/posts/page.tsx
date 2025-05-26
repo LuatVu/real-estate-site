3623f29d0740e323ee28ec2fd978d4de1aa114cb
Vu Cong Luat (MS/EET23) <vlc1hc@bosch.com>
2025-05-26 17:35:56 +0700

    developing posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -7,9 +7,14 @@ import MbFooter from "../ui/mobile/footer/mb.footer";
 import DownloadApp from "../ui/mobile/download-app/mb.download";
 import ExtraInfo from "../ui/mobile/extra-info/mb.extra.info";
 import Link from "next/link";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+
+
 
 export default function Posts() {
     const screenSize = useScreenSize();
+
     return (
         <div>
             {screenSize === 'sm' ? (<MobilePosts />) : (<DesktopPosts />)}
@@ -19,26 +24,45 @@ export default function Posts() {
 }
 
 function MobilePosts() {
+    const images = [
+        {
+            url: "/temp/Frame_7.jpg",
+            caption: "Image for test"
+        },
+        {
+            url: "/temp/Frame_7.jpg",
+            caption: "Image for test"
+        },
+        {
+            url: "/temp/Frame_7.jpg",
+            caption: "Image for test"
+        },
+        {
+            url: "/temp/Frame_7.jpg",
+            caption: "Image for test"
+        },
+        {
+            url: "/temp/Frame_7.jpg",
+            caption: "Image for test"
+        },
+        {
+            url: "/temp/Frame_7.jpg",
+            caption: "Image for test"
+        }
+    ];
+
     return (
         <div className="h-full">
-            <div className={styles.rootContainer}>         
+            <div className={styles.rootContainer}>
                 <NavBarMobile displayNav={true} />
                 <div className={styles.postContainer}>
-                    <div className={styles.imageBlk}>
-                        <Link href={"/123423232-232"} className={styles.post}>
-                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
-                        </Link>
-                        <Link href={"/123423232-232"} className={styles.post}>
-                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
-                        </Link>
-                        <Link href={"/123423232-232"} className={styles.post}>
-                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
-                        </Link>
-                        <Link href={"/123423232-232"} className={styles.post}>
-                            <Image className={styles.postChild} width={197} height={100} alt="" src="/temp/Frame_7.jpg" />                                
-                        </Link>                                           
+                    <div className="slide-container">                                                    
+                        <Carousel>
+                            {images.map((img, index) => (                                                                    
+                                <Image width={197} height={100} alt="" src={img.url} />                                
+                            ))}
+                        </Carousel>                        
                     </div>
-
                     <div>
                         <h1>Nhà mới đẹp ở ngay, HTX vào nhà Nguyễn Văn Nghị P7 Gò Vấp, 3PN 7.7 tỷ</h1>
                         <p>Thông tin mô tả</p>
