4b35d9ae0fe73c8a3e83a9dcaa2c0ea1fd00e392
Vu Cong Luat (MS/EET34) <vlc1hc@bosch.com>
2025-06-10 18:55:07 +0700

    developing feature Property Type popup

--- app/ui/common/pop-up-property-type/property-type-popup.tsx
+++ app/ui/common/pop-up-property-type/property-type-popup.tsx
@@ -2,12 +2,18 @@ import Image from "next/image";
 import styles from "./popup.module.css";
 import Form from "next/form";
 
-export default function PropertyTypePopup({onClose }: any) {
-    const submit = () =>{
+export default function PropertyTypePopup({ onClose }: any) {
+
+    const properTypes = [{ name: "Tất cả nhà bán", value: "ALL", checked: false }, { name: "Căn hộ chung cư", value: "CHCC", checked: false }, { name: "Nhà riêng, biệt thự, nhà phố", value: "NHA_RIENG", checked: false },
+    { name: "Đất nền", value: "DAT_NEN", checked: false }, { name: "Condotel", value: "CONDOTEL", checked: false },
+    { name: "Kho, nhà xưởng", value: "KHO_NHA_XUONG", checked: false }, { name: "Bất động sản khác", value: "BDS_KHAC", checked: false }        
+    ];
+
+    const submit = () => {
 
     }
     return (
-        <Form action={submit} className={styles.container}>            
+        <Form action={submit} className={styles.container}>
             <div className={styles.headerPopup}>
                 <div className={styles.loiBtNgSnParent}>
                     <div className={styles.loiBtNg}>Loại bất động sản</div>
@@ -17,62 +23,18 @@ export default function PropertyTypePopup({onClose }: any) {
                 </div>
             </div>
             <div className={styles.bodyPopup}>
-                <div className={styles.frameParent}>
-                    <div className={styles.buildingapartmentParent}>
-                        <Image className={styles.buildingapartmentIcon} width={16} height={16} alt="" src="/icons/BuildingApartment.svg" />
-                        <div className={styles.ttCNh}>Tất cả nhà bán</div>
-                    </div>                  
-                    <input type="checkbox" name="all_property" value="all_property" className={styles.frameItem} />
-                    <div className={styles.lineDiv} />
-                </div>
-                <div className={styles.frameGroup}>
-                    <div className={styles.buildingParent}>
-                        <Image className={styles.buildingapartmentIcon} width={16} height={16} alt="" src="/icons/Building.svg" />
-                        <div className={styles.cnHChung}>Căn hộ chung cư</div>
+                {properTypes.map((element: any) => (
+                    <div className={styles.frameParent} key={element.value}>
+                        <div className={styles.buildingapartmentParent}>
+                            <Image className={styles.buildingapartmentIcon} width={16} height={16} alt="" src="/icons/BuildingApartment.svg" />
+                            <p>{element.name}</p>
+                        </div>
+                        <div className={styles.checkboxBlock}>
+                            <input type="checkbox" name={element.value} value={element.value} className={styles.checkbox} />
+                        </div>                                                
                     </div>
-                    <input type="checkbox" name="CC" value="CC" className={styles.frameInner} />
-                    <div className={styles.lineDiv} />
-                </div>
-                <div className={styles.frameContainer}>
-                    <div className={styles.buildingParent}>
-                        <Image className={styles.buildingapartmentIcon} width={16} height={16} alt="" src="/icons/HouseLine.svg" />
-                        <div className={styles.nhRingBit}>Nhà riêng, biệt thự, nhà phố, shophouse</div>
-                    </div>
-                    <input type="checkbox" name="shophouse" value="shophouse" className={styles.frameInner} />
-                    <div className={styles.lineDiv} />
-                </div>
-                <div className={styles.frameParent1}>
-                    <div className={styles.buildingParent}>
-                        <Image className={styles.vectorIcon} width={16} height={12} alt="" src="/icons/BaseSquare.svg" />
-                        <div className={styles.nhRingBit}>Đất nền</div>
-                    </div>
-                    <input type="checkbox" name="basesquare" value="basesquare" className={styles.frameInner} />
-                    <div className={styles.lineDiv} />
-                </div>
-                <div className={styles.frameParent2}>
-                    <div className={styles.buildingParent}>
-                        <Image className={styles.buildingapartmentIcon} width={16} height={16} alt="" src="/icons/Condotel.svg" />
-                        <div className={styles.nhRingBit}>Condotel</div>
-                    </div>
-                    <input type="checkbox" name="condotel" value="condotel" className={styles.frameInner} />
-                    <div className={styles.lineDiv} />
-                </div>
-                <div className={styles.frameParent3}>
-                    <div className={styles.buildingParent}>
-                        <Image className={styles.buildingapartmentIcon} width={16} height={16} alt="" src="/icons/Warehouse.svg" />
-                        <div className={styles.nhRingBit}>Kho, nhà xưởng</div>
-                    </div>
-                    <input type="checkbox" name="warehouse" value="warehouse" className={styles.frameInner} />
-                    <div className={styles.lineDiv} />
-                </div>
-                <div className={styles.frameParent4}>
-                    <div className={styles.buildingParent}>
-                        <Image className={styles.buildingapartmentIcon} width={16} height={16} alt="" src="/icons/OtherProperty.svg" />
-                        <div className={styles.nhRingBit}>Bất động sản khác</div>
-                    </div>
-                    <input type="checkbox" name="others" value="others" className={styles.frameInner} />
-                    <div className={styles.lineDiv} />
-                </div>
+                ))}
+
             </div>
             <div className={styles.footerPopup}>
                 <button type="submit" className={styles.btnApply}>Áp dụng</button>
