9fe5ed004aee33cd86bdc232af24c758b5bd0bb6
Vu Cong Luat (MS/EET34) <vlc1hc@bosch.com>
2025-06-25 10:38:58 +0700

    module searching section

--- app/home/mobile-home.tsx
+++ app/home/mobile-home.tsx
@@ -6,8 +6,8 @@ import MbFooter from "../ui/mobile/footer/mb.footer";
 import NavBarMobile from '../ui/mobile/navigation/nav-bar-mobile';
 import Link from "next/link";
 import FilterPopup from '../ui/common/popup-filter/filter-popup';
-import Form from 'next/form';
 import { useRouter } from 'next/navigation';
+import SearchingSector from '../ui/mobile/searching-sector/searching.sector';
 
 export default function MobileHome() {
     const router = useRouter()
@@ -109,30 +109,9 @@ export default function MobileHome() {
             {filterPopup && (<FilterPopup onClose={closeFilterPopup} setFilterParam={setFilterParam} filterParam={searchRequest} />)}            
             {homePageVisible && (
                 <div className={styles.homePage}>
-                    <NavBarMobile displayNav={false} />
-                    <Form action={search} className={styles.searchingsession}>
-                        <div className={styles.searchFieldsParent}>
-                            <div className={styles.searchFields}>
-                                <div className={styles.frameParent}>
-                                    <div className={styles.placeHolderWrapper}>
-                                        <input name="searchKeyword" className={styles.inputText} placeholder="Nhập thông tin bất kỳ ..." />
-                                    </div>
-                                    <button type="submit" className={styles.button8}>
-                                        <Image className={styles.searchIcon} width={24} height={24} alt="" src="/icons/search.svg" />
-                                    </button>
-                                </div>
-                            </div>
-                            <div className={styles.filterGroup}>
-                                <button className={styles.button9} onClick={openFilterPopup}>
-                                    <Image className={styles.funnelIcon} width={24} height={24} alt="" src="/icons/Funnel.svg" />
-                                    <div className={styles.filter}>Lọc</div>
-                                    <div>
-                                        {filterNum== 0?(<p className={styles.filterNumZero +" " + styles.wrapper}>{filterNum}</p>): (<p className={styles.filterNum +" " + styles.wrapper}>{filterNum}</p>)}
-                                    </div>
-                                </button>                                
-                            </div>
-                        </div>
-                    </Form>
+                    <NavBarMobile displayNav={false} />                    
+                    <SearchingSector search={search} openFilterPopup={openFilterPopup} filterNum={filterNum}/>
+
                     <div >
                         <Image className={styles.imagesIcon} width={393} height={141} alt="" src="/icons/Header_Image.png" />
                     </div>
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -4,11 +4,11 @@ import useScreenSize from '../lib/useScreenSize';
 import { useState, useCallback, useEffect } from 'react';
 import FilterPopup from '../ui/common/popup-filter/filter-popup';
 import NavBarMobile from '../ui/mobile/navigation/nav-bar-mobile';
-import Form from 'next/form';
 import Image from 'next/image';
 import Link from 'next/link';
 import MbFooter from '../ui/mobile/footer/mb.footer';
 import { useSearchParams  } from 'next/navigation';
+import SearchingSector from '../ui/mobile/searching-sector/searching.sector';
 
 export default function Posts() {
     const screenSize = useScreenSize();
@@ -142,29 +142,7 @@ function PostsOnMobile() {
             {homePageVisible && (
                 <div className={styles.homePage}>
                     <NavBarMobile displayNav={false} />
-                    <Form action={search} className={styles.searchingsession}>
-                        <div className={styles.searchFieldsParent}>
-                            <div className={styles.searchFields}>
-                                <div className={styles.frameParent}>
-                                    <div className={styles.placeHolderWrapper}>
-                                        <input name="searchKeyword" className={styles.inputText} placeholder="Nhập thông tin bất kỳ ..." />
-                                    </div>
-                                    <button type="submit" className={styles.button8}>
-                                        <Image className={styles.searchIcon} width={24} height={24} alt="" src="/icons/search.svg" />
-                                    </button>
-                                </div>
-                            </div>
-                            <div className={styles.filterGroup}>
-                                <button className={styles.button9} onClick={openFilterPopup}>
-                                    <Image className={styles.funnelIcon} width={24} height={24} alt="" src="/icons/Funnel.svg" />
-                                    <div className={styles.filter}>Lọc</div>
-                                    <div>
-                                        {filterNum == 0 ? (<p className={styles.filterNumZero + " " + styles.wrapper}>{filterNum}</p>) : (<p className={styles.filterNum + " " + styles.wrapper}>{filterNum}</p>)}
-                                    </div>
-                                </button>
-                            </div>
-                        </div>
-                    </Form>
+                    <SearchingSector search={search} openFilterPopup={openFilterPopup} filterNum={filterNum}/>
                     <div className={styles.firstMainContent}>
                         <div className={styles.postParent}>
                             {posts.map((element: any) => (
