493df051eec2ae4bbc5f5b7b4747f8e77aadc668
Vu Cong Luat (MS/EET34) <vlc1hc@bosch.com>
2025-07-04 18:05:16 +0700

    modify Sign-in screen

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 import styles from './index.module.css';
 import MbFooter from "../ui/mobile/footer/mb.footer";
 import Button from "../ui/common/button/button";
-import Submit from "../ui/common/button/submit";
 import btnstyle from '../ui/common/button/btn.module.css';
 import Link from 'next/link';
+import Form from 'next/form';
 
 export default function SignIn() {
     const screenSize = useScreenSize();
     return (
-        <div>
+        <div className="h-full">
             {screenSize === 'sm' ? (<MobileSignIn />) : (<DesktopSignIn />)}
         </div>
     );
@@ -19,36 +19,39 @@ export default function SignIn() {
 
 function MobileSignIn() {
     return (
-        <main>
-            <div className={styles.signIn}>
-                <Image className={styles.headerImageIcon} width={393} height={151} alt="" src="/icons/Header_Image.png" />
-                <form className={styles.loginFormContainer} action="/sign-in-action">
+        <div className="h-full">
+            <div className={styles.container}>
+                <div>
+                    <Image className={styles.headerImageIcon} width={393} height={151} alt="" src="/icons/Header_Image.png" />
+                </div>
+
+                <Form className={styles.loginFormContainer} action="/sign-in-action">
                     <div className={styles.loginFormTitle}>
-                        <div className={styles.headingTitle}>Đăng nhập để tiếp tục</div>
+                        <p>Đăng nhập để tiếp tục</p>
                     </div>
                     <div className={styles.inputFieldsContainer}>
                         <div className={styles.inputFieldsContainerInner}>
                             <div className={styles.userParent}>
                                 <Image className={styles.userIcon} width={24} height={24} alt="" src="/icons/useIcon.svg" />
-                                <input className={styles.inputText} placeholder="Số điện thoại"/>
+                                <input className={styles.inputText} placeholder="Số điện thoại" />
                             </div>
                         </div>
-                    </div>
-                    <div className={styles.loginFormContainerInner}>
-                        <div className={styles.frameParent}>
+                        <div className={styles.inputFieldsContainerInner}>
                             <div className={styles.userParent}>
                                 <Image className={styles.userIcon} width={24} height={24} alt="" src="/icons/lockIcon.svg" />
-                                <input className={styles.inputText} placeholder="Mật khẩu" type="password"/>
+                                <input className={styles.inputText} placeholder="Mật khẩu" type="password" />
                             </div>
                             <Image className={styles.userIcon} width={24} height={24} alt="" src="/icons/eyeIcon.svg" />
                         </div>
                     </div>
-                    <Link href="/recover-passwod" className={styles.forgotPassword}>Quên mật khẩu?</Link>
-                    <Submit text="Đăng nhập" cssClass={[styles.button, btnstyle.buttonprimary]} onClick={() => console.log("Click")} />
-                </form>
+                    <div>
+                        <Link href="/recover-passwod" className={styles.forgotPassword}>Quên mật khẩu?</Link>
+                    </div>
+                    <button type="submit" className={styles.btnSubmit}>Đăng nhập</button>
+                </Form>
                 <div className={styles.dividerWithText}>
+                    <div className={styles.dividerWithTextItem} />
                     <div className={styles.dividerText}>Hoặc</div>
-                    <div className={styles.dividerWithTextChild} />
                     <div className={styles.dividerWithTextItem} />
                 </div>
                 <div className={styles.socialLoginContainer}>
@@ -59,9 +62,9 @@ function MobileSignIn() {
                         <Link href="/sign-up" className={styles.ngK}>Đăng Ký</Link>
                     </div>
                 </div>
+                <MbFooter />
             </div>
-            <MbFooter />
-        </main>
+        </div>
 
     );
 }
